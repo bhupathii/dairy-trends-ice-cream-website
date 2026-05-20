c0094281188725e8b6aa1f193492c6e1319bb587
Bhupathi Reddy <vtu23091@veltech.edu.in>
2026-05-20 22:44:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self'; frame-src 'self' https://www.google.com;"
           },
           {
             key: 'X-Frame-Options',
